7493e57ab10ac204a1facc8d87775c9083b2e72c
Thiago Kasper Souza <thiagokaspersouza@gmail.com>
2024-11-28 22:01:57 -0300

    feat: Updates router

--- router.js
+++ router.js
@@ -1,7 +1,18 @@
  // Função para carregar o conteúdo do nav.html
- fetch('./src/components/main.html')
- .then(response => response.text())
- .then(data => {
-   document.getElementById('main-placeholder').innerHTML = data;
- })
- .catch(error => console.error('Erro ao carregar o main:', error));
+ switch(window.location.pathname) {
+  case "/":
+    fetch('./src/components/main.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('main-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Erro ao carregar o main:', error));
+    break;
+  default:
+    fetch('./src/components/main.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('main-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Erro ao carregar o main:', error));
+ }
