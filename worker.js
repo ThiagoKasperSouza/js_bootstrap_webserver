375baee374d897b0b7b407750872b1e4eb867301
Thiago Kasper Souza <thiagokaspersouza@gmail.com>
2024-12-03 21:29:57 -0300

    feat: Updates broker

--- worker.js
+++ worker.js
@@ -1,14 +1,18 @@
 // worker.js
 
 self.onmessage = function(event) {
+    var client = mqtt.connect('ws://broker.mqttdashboard.com:8000/mqtt')
+    client.subscribe("mqtt/test")
 
-    const start = Date.now();
+    client.on("message", function (topic, payload) {
+      console.log([topic, payload].join(": "))
+        client.end()
+    })
 
+    
 
     for (let i = 0; true; i++) {
-        const millis = Date.now() - start;
-
-        console.log(`seconds elapsed = ${Math.floor(millis / 1000)}`);
+        client.publish("mqtt/test", "hello world!")
     }
 
     // Envie o resultado de volta para o thread principal
